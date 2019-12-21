8ee878585ac3d8326defc2090442c7598c66acd9
LaiJM <2536226456@qq.com>
2019-12-21 19:35:07 +0800

    登录 注册 修改个人信息

--- src/main/java/com/bbs/controller/lai/UserController.java
+++ src/main/java/com/bbs/controller/lai/UserController.java
@@ -8,10 +8,9 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(value = "用户控制器，包含用户的登录、注册")
 @RestController
@@ -36,5 +35,33 @@ public class UserController {
             return new ResponseEntity<>(ResultFactory.buildFailResult("失败"), HttpStatus.OK);
         }
     }
+
+
+    @GetMapping(path = "/user/{id}")
+    public ResponseEntity<Result> getUser(@PathVariable("id")Integer id) {
+        List<User> list = userService.findByUserid(id);
+        User user = list.get(0);
+        return new ResponseEntity<>(ResultFactory.buildSuccessResult(user),HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/user/{id}")
+    public ResponseEntity<Result> updateUser(@RequestBody User user) {
+        if(userService.update(user) == 1) {
+            return new ResponseEntity<>(ResultFactory.buildSuccessResult("成功"), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(ResultFactory.buildFailResult("失败"), HttpStatus.OK);
+        }
+    }
+
+
+    @PostMapping(path = "/user")
+    public ResponseEntity<Result> register(@RequestBody User user) {
+        if (userService.registerUser(user)) {
+            return new ResponseEntity<>(ResultFactory.buildSuccessResult("注册成功"),HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(ResultFactory.buildFailResult("失败"), HttpStatus.OK);
+        }
+    }
+
 
 }
